d31e457516d674bd177c70aa7138045648a7ea38
Tushar Ahire <tusharmadhavahire98@gmail.com>
2022-03-06 14:19:21 +0530

    final

---
+++ main.js
@@ -0,0 +1,9 @@
+let add=require ("./add")
+let divide=require ("./divide")
+let multiply=require ("./multiply")
+let subtract=require ("./subtract")
+
+console.log(add(5,5))
+console.log(divide(5,5))
+console.log(multiply(5,5))
+console.log(subtract(5,5))
